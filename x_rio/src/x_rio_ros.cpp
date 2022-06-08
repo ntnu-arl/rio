c6056a1378b90d5b9a6dfe7ad93db482f7b93cf8
Morten <morten.c.nissov@gmail.com>
2022-06-08 15:33:33 +0200

    sensor stick configs, modified for functioning without trigger

--- x_rio/src/x_rio_ros.cpp
+++ x_rio/src/x_rio_ros.cpp
@@ -483,6 +483,7 @@ void XRioRos::iterateRadarScan()
         {
           FullRadarCloneState radar_clone_state;
           uint clone_state_id;
+          x_rio_filter_.addRadarStateClone(0, radar_data_msg.header.stamp);
           if (x_rio_filter_.getFullRadarState(radar_data.first, radar_clone_state, clone_state_id))
           {
             const auto time_diff_clone =
